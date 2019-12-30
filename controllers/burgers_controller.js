ce6443a6c063083be11fc6928e66ef2493f59948
Sophie <sophie88leigh@gmail.com>
2019-12-30 12:06:13 -0500

    added instructions and server.js file

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,9 @@
+//Inside the burgers_controller.js file, import the following: Express, burger.js
+const Express = require("express");
+const burger = require("burger");
+
+
+//Create the router for the app, and export the router at the end of your file.
+
+
+module.exports = router
