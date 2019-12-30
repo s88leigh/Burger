98695176b83c07d694e19c897fc706a955f1fdbc
Sophie <sophie88leigh@gmail.com>
2019-12-30 11:43:22 -0500

    created intitial folders and files

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+--In the db folder, create a file named schema.sql. Write SQL queries this file that do the following:
+
+--Create the burgers_db.
+--Switch to or use the burgers_db.
+--Create a burgers table with these fields:
+
+
+--id: an auto incrementing int that serves as the primary key.
+
+--burger_name: a string.
+
+--devoured: a boolean.
+
+--Run the schema.sql and seeds.sql files into the mysql server from the command line
---
+++ models/burger.js
@@ -0,0 +1,7 @@
+//Inside burger.js, import orm.js into burger.js
+const orm = require("orm");
+
+//Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
+
+
+module.exports = burger
